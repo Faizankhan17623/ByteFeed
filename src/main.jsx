613e1ddd496e4420434104a861f106f6bb8b3bd8
FAIZAN KHAN <faizankhan901152@gmail.com>
2026-05-12 15:15:33 +0530

    feat: add BrowserRouter with routes for home and privacy policy page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
+import PrivacyPolicy from "./components/PrivacyPolicy.jsx";
 
 createRoot(document.getElementById("app")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
